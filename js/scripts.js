7db2647ea203aaa2228642b0946acdb3bb8765e3
Sean <crimsonsand@live.com>
2022-04-06 19:36:18 -0700

    adding branching to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,27 @@
-function add(number1, number2) {
-  return number1 + number2;
+var operators = ['+', '-', 'x', '/', '=', 'cl'];
+var buffer = '';
+var currentResult = 0;
+var currentOperation = '';
+
+function createKeys()
+{
+    // create the number pad
+	for(let i = 0; i < 10; i++)
+	{
+		let key = document.createElement('div');
+		key.innerHTML = i;
+		key.dataset.value = i;
+		key.className = 'key';
+
+		document.getElementById('numpad').appendChild(key);
+	}
+	// create the row of operators
+	operators.forEach(function(item){
+		let key = document.createElement('div');
+		key.innerHTML = item;
+		key.dataset.operation = item;
+		key.className = 'key';
+
+		document.getElementById('operators').appendChild(key);
+	});
 }
-
-function subtract(number1, number2) {
-  return number1 - number2;
-}
-
-function multiply(number1, number2) {
-  return number1 * number2;
-}
-
-function divide(number1, number2) {
-  return number1 / number2;
-}
-
-// Everything below this line is the user interface logic:
-
-$(document).ready(function() {
-  $("form1#add").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form2#subtract").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#subtract1").val());
-    const number2 = parseInt($("#subtract2").val());
-    const result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form3#multiply").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#multiply1").val());
-    const number2 = parseInt($("#multiply2").val());
-    const result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form4#divide").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#divide1").val());
-    const number2 = parseInt($("#divide2").val());
-    const result = divide(number1, number2);
-    $("#output").text(result);
-  });
-});
